8a3144f3e0e3968d53ec3dc81a3a743948bff641
hiennguyen <nguyentronghien.contact@gmail.com>
2021-12-20 11:18:12 +0700

    Change header and footer

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,8 +8,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
         integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <title>Mlem Cinema</title>
-    <link rel="icon" type="image/png" href="img/logo.png">
-</head>
+    </head>
 
 <body>
     <?php
@@ -24,59 +22,29 @@
 
         <div class="movies-container">
             <?php
-                        if($result = mysqli_query($link, $sql)){
-                            if(mysqli_num_rows($result) > 0){
-                                for ($i = 0; $i <= 5; $i++){
-                                    $row = mysqli_fetch_array($result);
-                                    echo '<div class="movie-box">';
-                                    echo '<img src="'. $row['movieImg'] .'" alt=" ">';
-                                    echo '<div class="movie-info ">';
-                                    echo '<h3>'. $row['movieTitle'] .'</h3>';
-                                    echo '<a href="booking.php?id='.$row['movieID'].'"><i class="fas fa-ticket-alt"></i> Book a seat</a>';
-                                    echo '</div>';
-                                    echo '</div>';
-                                }
-                                mysqli_free_result($result);
-                            } else{
-                                echo '<h4 class="no-annot">No Booking to our movies right now</h4>';
-                            }
-                        } else{
-                            echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+                if($result = mysqli_query($link, $sql)){
+                    if(mysqli_num_rows($result) > 0){
+                        for ($i = 0; $i <= 5; $i++){
+                            $row = mysqli_fetch_array($result);
+                            echo '<div class="movie-box">';
+                            echo '<img src="'. $row['movieImg'] .'" alt=" ">';
+                            echo '<div class="movie-info ">';
+                            echo '<h3>'. $row['movieTitle'] .'</h3>';
+                            echo '<a href="booking.php?id='.$row['movieID'].'"><i class="fas fa-ticket-alt"></i> Book a seat</a>';
+                            echo '</div>';
+                            echo '</div>';
                         }
-                        
-                        // Close connection
-                        mysqli_close($link);
-                        ?>
-        </div>
-    </div>
-    <div id="home-section-2" class="services-section">
-        <h1>How it works</h1>
-        <h3>3 Simple steps to book your favourit movie!</h3>
-
-        <div class="services-container">
-            <div class="service-item">
-                <div class="service-item-icon">
-                    <i class="fas fa-4x fa-video"></i>
-                </div>
-                <h2>1. Choose your favourite movie</h2>
-                <p>choose the movie you want to watch at our theaters</p>
-            </div>
-            <div class="service-item">
-                <div class="service-item-icon">
-                    <i class="fas fa-4x fa-credit-card"></i>
-                </div>
-                <h2>2. Pay for your tickets</h2>
-                <p>choose the movie you want to watch at our theaters</p>
-            </div>
-            <div class="service-item">
-                <div class="service-item-icon">
-                    <i class="fas fa-4x fa-theater-masks"></i>
-                </div>
-                <h2>3. Pick your seats & Enjoy watching</h2>
-                <p>choose the movie you want to watch at our theaters</p>
-            </div>
-            <div class="service-item"></div>
-            <div class="service-item"></div>
+                        mysqli_free_result($result);
+                    } else{
+                        echo '<h4 class="no-annot">No Booking to our movies right now</h4>';
+                    }
+                } else{
+                    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+                }
+                
+                // Close connection
+                mysqli_close($link);
+            ?>
         </div>
     </div>
     <div id="home-section-3" class="trailers-section">
@@ -132,5 +100,4 @@
     <script src="scripts/jquery-3.3.1.min.js "></script>
     <script src="scripts/script.js "></script>
 </body>
-
 </html>
